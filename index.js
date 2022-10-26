be07fb153848c1109c71513aff550070a64ec705
RoRomario360 <fotoromario@gmail.com>
2022-10-27 01:06:41 +0300

    backand_finish

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import express from 'express';
 import multer from 'multer'
 import mongoose from 'mongoose';
 import {registerValidation, loginValidation, postCreateValidation} from "./validations/validation.js";
-import checkAuth from './utils/checkAuth.js'
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js";
-
+import {checkAuth,handleValidationErrors} from './utils/index.js'
+import {PostController,UserController} from './controllers/index.js'
 
 
 mongoose.connect('mongodb+srv://nodeAdmin:wwwwww@cluster0.e3vbbqh.mongodb.net/blog?retryWrites=true&w=majority').then(() => console.log('DB OK'))
@@ -14,24 +12,33 @@ mongoose.connect('mongodb+srv://nodeAdmin:wwwwww@cluster0.e3vbbqh.mongodb.net/bl
 const app = express();
 
 const storage=multer.diskStorage({
-    destination:(_, _, cb)=>{
+    destination:(_, __, cb)=>{
       cb(null,'upload')
-    }
+    },
+    filename:(_, file, cb)=>{
+        cb(null,file.originalname)
+    },
 })
+const upload=multer({storage})
 
 app.use(express.json());
-
+app.use('/uploads',express.static('uploads'))
 //User
-app.post('auth/login',loginValidation,UserController.login)
-app.post('/auth/register', registerValidation,UserController.register );
+app.post('auth/login',loginValidation,handleValidationErrors,UserController.login)
+app.post('/auth/register', registerValidation,handleValidationErrors,UserController.register );
 app.get('/auth/me',checkAuth,UserController.getMe );
 
+app.post('/upload',checkAuth,upload.single('image'),(req,res)=>{
+    res.json({
+        url:`/uploads/${req.file.originalname}`
+    })
+})
 //Posts
 app.get('/posts',PostController.getAll)
 app.get('/posts:id',PostController.getOne)
-app.post('/posts',postCreateValidation,PostController.create)
+app.post('/posts',checkAuth,postCreateValidation,handleValidationErrors,PostController.create)
 app.delete('/posts/:id',checkAuth,PostController.remove)
-app.path('/posts/:id',checkAuth,PostController.update)
+app.path('/posts/:id',checkAuth,postCreateValidation,handleValidationErrors,PostController.update)
 
 app.listen(4444, (err) => {
     if (err) {
